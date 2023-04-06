e5d5584d91593965b2d4a0319a25f9e08e2a3f84
Bahadur Khan Swati <57499169+BahadurKhan10@users.noreply.github.com>
2023-04-06 18:23:25 +0500

    Add files via upload

---
+++ dist/app.js
@@ -0,0 +1,177 @@
+"use strict";
+// Typescript Tasks
+// 1. Write a JavaScript program that accepts two numbers in two prompts and
+// displays the larger one in the console.
+function largerNum() {
+    var _a, _b;
+    const num1 = parseInt(((_a = prompt("Enter number 1")) === null || _a === void 0 ? void 0 : _a.trim()) || ''); //Handled null or undefined
+    const num2 = parseInt(((_b = prompt("Enter Number 2")) === null || _b === void 0 ? void 0 : _b.trim()) || ''); //Handled null or undefined
+    if (isNaN(num1) || isNaN(num2)) {
+        return "Invalid input!";
+    }
+    else if (num1 > num2) {
+        return `${num1} is greater than ${num2}`;
+    }
+    else if (num2 > num1) {
+        return `${num2} is greater than ${num1}`;
+    }
+    else {
+        return `${num1} is equal to ${num2}`;
+    }
+}
+const comparisonResult = largerNum();
+console.log(comparisonResult);
+// -----------------------------------------------------------------------------------------------------------------
+// 2. Write a JavaScript conditional statement to find the sign of a number. Display
+// an alert box with the specified sign.
+// Sample: 3
+// Output : The sign is +
+// Sample: -5
+// Output : The sign is -
+const num = -5;
+if (num < 0) {
+    // console.log("The sign is -")
+    console.log(`${num} is negative`);
+}
+else {
+    // console.log("The sign is +")
+    console.log(`${num} is positive`);
+}
+// Making Function for above
+function checkSign(num) {
+    if (num < 0) {
+        // console.log("The sign is -")
+        return `${num} is negative`;
+    }
+    else {
+        // console.log("The sign is +")
+        return `${num} is posive`;
+    }
+}
+console.log(checkSign(5));
+console.log(checkSign(-5));
+console.log(checkSign(3));
+console.log(checkSign(-3));
+// -----------------------------------------------------------------------------------------------------------------
+// 3. Write a JavaScript program that accepts five numbers in five prompts and
+// displays the larger one in the console
+function checkLargeNum() {
+    var _a, _b, _c, _d, _e;
+    const numb1 = parseInt(((_a = prompt("Enter number 1")) === null || _a === void 0 ? void 0 : _a.trim()) || '');
+    const numb2 = parseInt(((_b = prompt("Enter Number 2")) === null || _b === void 0 ? void 0 : _b.trim()) || '');
+    const numb3 = parseInt(((_c = prompt("Enter number 3")) === null || _c === void 0 ? void 0 : _c.trim()) || '');
+    const numb4 = parseInt(((_d = prompt("Enter Number 4")) === null || _d === void 0 ? void 0 : _d.trim()) || '');
+    const numb5 = parseInt(((_e = prompt("Enter number 5")) === null || _e === void 0 ? void 0 : _e.trim()) || '');
+    const max = Math.max(numb1, numb2, numb3, numb4, numb5);
+    return `The larger number is ${max}`;
+    // console.log(`The larger number is ${max}`)
+}
+const x = checkLargeNum();
+console.log(x);
+// -----------------------------------------------------------------------------------------------------------------
+// 4. Write a JavaScript for loop that will iterate from 0 to 15. For each iteration, it
+// will check if the current number is odd or even, and display a message to the
+// screen.
+// Sample Output :
+// "0 is even"
+// "1 is odd"
+// "2 is even"
+let i;
+for (i = 0; i <= 15; i = i + 1) {
+    if (i % 2 === 0) {
+        console.log(`${i} is even`);
+    }
+    else {
+        console.log(`${i} is odd`);
+    }
+}
+// -----------------------------------------------------------------------------------------------------------------
+// 5. Write a JavaScript program which computes the average marks of the
+// following students Then, this average is used to determine the corresponding
+// grade.
+// Range Grade
+// <60     F
+// <70     D
+// <80     C
+// <90     B
+// <100    A
+function calculateAverageGrade() {
+    var _a, _b, _c, _d, _e;
+    let grades = [];
+    let chemistryGrade = parseInt(((_a = prompt("Enter your Chemistry grade:")) === null || _a === void 0 ? void 0 : _a.trim()) || '');
+    if (!isNaN(chemistryGrade)) {
+        grades.push(chemistryGrade);
+    }
+    let englishGrade = parseInt(((_b = prompt("Enter your English grade:")) === null || _b === void 0 ? void 0 : _b.trim()) || '');
+    if (!isNaN(englishGrade)) {
+        grades.push(englishGrade);
+    }
+    let mathsGrade = parseInt(((_c = prompt("Enter your Maths grade:")) === null || _c === void 0 ? void 0 : _c.trim()) || '');
+    if (!isNaN(mathsGrade)) {
+        grades.push(mathsGrade);
+    }
+    let physicsGrade = parseInt(((_d = prompt("Enter your Physics grade:")) === null || _d === void 0 ? void 0 : _d.trim()) || '');
+    if (!isNaN(physicsGrade)) {
+        grades.push(physicsGrade);
+    }
+    let urduGrade = parseInt(((_e = prompt("Enter your Urdu grade:")) === null || _e === void 0 ? void 0 : _e.trim()) || '');
+    if (!isNaN(urduGrade)) {
+        grades.push(urduGrade);
+    }
+    const sum = grades.reduce((acc, curr) => acc + curr, 0);
+    const avg = sum / grades.length;
+    if (avg < 60) {
+        return "Grade is F";
+    }
+    else if (avg < 70 && avg >= 60) {
+        return "Grade is D";
+    }
+    else if (avg < 80 && avg >= 70) {
+        return "Grade is C";
+    }
+    else if (avg < 90 && avg >= 80) {
+        return "Grade is B";
+    }
+    else if (avg <= 100 && avg >= 90) {
+        return "Grade is A";
+    }
+}
+// -----------------------------------------------------------------------------------------------------------------
+// 6. Write a JavaScript program which iterates the integers from 1 to 100. But for
+// multiples of three print "Fizz" instead of the number and for the multiples of five
+// print "Buzz". For numbers which are multiples of both three and five print
+// "FizzBuzz".
+let n;
+for (let n = 1; n <= 100; n++) {
+    if (n % 3 === 0 && n % 5 === 0) {
+        console.log("FizzBuzz");
+    }
+    else if (n % 3 === 0) {
+        console.log("Fizz");
+    }
+    else if (n % 5 === 0) {
+        console.log("Buzz");
+    }
+    else {
+        console.log(n);
+    }
+}
+// -----------------------------------------------------------------------------------------------------------------
+// 7. Write a JavaScript program to construct the following pattern, using a nested
+// for loop.
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+let a;
+let b;
+for (let a = 1; a <= 5; a++) {
+    let line = '';
+    for (let b = 1; b <= i; b++) {
+        line += '* ';
+    }
+    console.log(line);
+}
+// -----------------------------------------------------------------------------------------------------------------
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,208 @@
+// Typescript Tasks
+
+
+
+// 1. Write a JavaScript program that accepts two numbers in two prompts and
+// displays the larger one in the console.
+
+function largerNum(): string {
+  const num1: number = parseInt(prompt("Enter number 1")?.trim() || ''); //Handled null or undefined
+  const num2: number = parseInt(prompt("Enter Number 2")?.trim() || ''); //Handled null or undefined
+
+  if (isNaN(num1) || isNaN(num2)) {
+    return "Invalid input!";
+  } else if (num1 > num2) {
+    return `${num1} is greater than ${num2}`;
+  } else if (num2 > num1) {
+    return `${num2} is greater than ${num1}`;
+  } else {
+    return `${num1} is equal to ${num2}`;
+  }
+}
+
+const comparisonResult: string = largerNum();
+console.log(comparisonResult);
+
+
+// -----------------------------------------------------------------------------------------------------------------
+
+// 2. Write a JavaScript conditional statement to find the sign of a number. Display
+// an alert box with the specified sign.
+// Sample: 3
+// Output : The sign is +
+// Sample: -5
+// Output : The sign is -
+
+
+const num:number=-5
+if (num < 0) {
+  // console.log("The sign is -")
+  console.log(`${num} is negative`) ;
+} else {
+  // console.log("The sign is +")
+  console.log(`${num} is positive`);
+}
+
+
+// Making Function for above
+
+function checkSign(num:number):string{
+    if(num < 0 ){
+        // console.log("The sign is -")
+        return `${num} is negative`
+
+    }else{
+        // console.log("The sign is +")
+        return `${num} is posive`
+
+    }
+}
+console.log(checkSign(5))
+console.log(checkSign(-5))
+console.log(checkSign(3))
+console.log(checkSign(-3))
+
+
+// -----------------------------------------------------------------------------------------------------------------
+
+// 3. Write a JavaScript program that accepts five numbers in five prompts and
+// displays the larger one in the console
+
+function checkLargeNum():string{
+    const numb1=parseInt(prompt("Enter number 1")?.trim() || '')
+    const numb2=parseInt(prompt("Enter Number 2")?.trim() || '')
+    const numb3=parseInt(prompt("Enter number 3")?.trim() || '')
+    const numb4=parseInt(prompt("Enter Number 4")?.trim() || '')
+    const numb5=parseInt(prompt("Enter number 5")?.trim() || '')
+
+    const max:number= Math.max(numb1,numb2,numb3,numb4,numb5)
+    return `The larger number is ${max}`
+    // console.log(`The larger number is ${max}`)
+}
+
+const x:string=checkLargeNum()
+console.log(x)
+
+// -----------------------------------------------------------------------------------------------------------------
+
+// 4. Write a JavaScript for loop that will iterate from 0 to 15. For each iteration, it
+// will check if the current number is odd or even, and display a message to the
+// screen.
+// Sample Output :
+// "0 is even"
+// "1 is odd"
+// "2 is even"
+
+let i:number
+for (i = 0; i <= 15; i = i + 1) {
+    if (i % 2 === 0) {
+      console.log(`${i} is even`);
+    } else {
+      console.log(`${i} is odd`);
+    }
+  }
+
+  // -----------------------------------------------------------------------------------------------------------------
+
+// 5. Write a JavaScript program which computes the average marks of the
+// following students Then, this average is used to determine the corresponding
+// grade.
+
+// Range Grade
+// <60     F
+// <70     D
+// <80     C
+// <90     B
+// <100    A
+
+
+function calculateAverageGrade(): string | undefined{
+    let grades = [];
+  
+    let chemistryGrade = parseInt(prompt("Enter your Chemistry grade:")?.trim() || '');
+    if (!isNaN(chemistryGrade)) {
+        grades.push(chemistryGrade);
+    }
+  
+    let englishGrade = parseInt(prompt("Enter your English grade:")?.trim() || '');
+    if (!isNaN(englishGrade)) {
+        grades.push(englishGrade);
+    }
+  
+    let mathsGrade = parseInt(prompt("Enter your Maths grade:")?.trim() || '');
+    if (!isNaN(mathsGrade)) {
+        grades.push(mathsGrade);
+    }
+  
+    let physicsGrade = parseInt(prompt("Enter your Physics grade:")?.trim() || '');
+    if (!isNaN(physicsGrade)) {
+        grades.push(physicsGrade);
+    }
+  
+    let urduGrade = parseInt(prompt("Enter your Urdu grade:")?.trim() || '');
+    if (!isNaN(urduGrade)) {
+        grades.push(urduGrade);
+    }
+  
+    const sum = grades.reduce((acc, curr) => acc + curr, 0);
+    const avg = sum / grades.length;
+  
+    if (avg < 60) {
+        return "Grade is F";
+      } else if (avg < 70 && avg >= 60) {
+        return "Grade is D";
+      } else if (avg < 80 && avg >= 70) {
+        return "Grade is C";
+      } else if (avg < 90 && avg >= 80) {
+        return "Grade is B";
+      } else if (avg <= 100 && avg >= 90) {
+        return "Grade is A";
+      }
+  }
+
+  
+// -----------------------------------------------------------------------------------------------------------------
+
+// 6. Write a JavaScript program which iterates the integers from 1 to 100. But for
+// multiples of three print "Fizz" instead of the number and for the multiples of five
+// print "Buzz". For numbers which are multiples of both three and five print
+// "FizzBuzz".
+
+let n:number
+for(let n=1; n<=100; n++){
+    if(n%3===0 && n%5===0){
+      console.log("FizzBuzz")
+    }else if(n%3===0){
+      console.log("Fizz")
+    }else if(n%5===0){
+      console.log("Buzz")
+    }else{
+      console.log(n)
+    }
+  }
+  
+  
+  
+  // -----------------------------------------------------------------------------------------------------------------
+  
+  // 7. Write a JavaScript program to construct the following pattern, using a nested
+  // for loop.
+  // *
+  // * *
+  // * * *
+  // * * * *
+  // * * * * *
+  
+  let a:number
+  let b:number
+  for(let a=1; a<=5; a++){
+    let line = '';
+    for(let b=1; b<=i; b++){
+      line += '* ';
+    }
+    console.log(line);
+  }
+  
+  
+  // -----------------------------------------------------------------------------------------------------------------
+  
